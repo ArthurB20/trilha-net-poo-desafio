724e45bdbaa62431dd6307fbf4be4b19dcc1c539
ArthurB <arthurb.andras@gmail.com>
2024-01-12 16:38:35 -0300

    Desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
@@ -21,6 +27,9 @@ namespace DesafioPOO.Models
             Console.WriteLine("Recebendo ligação...");
         }
 
-        public abstract void InstalarAplicativo(string nomeApp);
+        public virtual void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando: {nomeApp}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("123456", "Nokia 1", "8", 46);
+nokia.InstalarAplicativo("Shazam");
+nokia.Ligar();
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone IPhone:");
+Iphone iphone = new Iphone("78910", "IPhone 7", "3", 58);
+iphone.InstalarAplicativo("Reddit");
+iphone.ReceberLigacao();
